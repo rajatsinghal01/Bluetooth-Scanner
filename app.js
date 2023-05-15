ee8c2cc7191c822966a6548255d7682485714b9b
Rajat Singhal <RajatSinghalm@gmail.com>
2023-05-15 16:31:51 +0530

    Connection Done

--- app.js
+++ app.js
@@ -1,7 +1,69 @@
 var button = document.getElementById("btn_scan");
 var Bluetooth_Table = document.getElementById("bluetooth_Table");
+var isBluetoothPresent = false;
+var isConnected = false;
+var bluetoothDevice = null;
 
 function ClickedButton() {
-    document.getElementById("para_sample").innerHTML = "Hi You Clicked the button";
-    Bluetooth_Table.rows.item(1).cells.item(1).innerHTML = "Connecting";
+
+    if (isConnected) {
+
+        Disconnect();
+    }
+    else {
+
+        // Checking Bluetooth Availability
+        navigator.bluetooth.getAvailability().then((available) => {
+            if (available) {
+                isBluetoothPresent = true;
+                console.log("This device supports Bluetooth!");
+                Connect_to_Bluetooth();
+
+            } else {
+                console.log("Bluetooth is not supported! ");
+                Bluetooth_Table.rows.item(1).cells.item(1).innerHTML = "Device does not supports Bluetooth";
+                isBluetoothPresent = false
+            }
+        });
+    }
+
+
+}
+
+function Connect_to_Bluetooth() {
+    // Connecting Bluetooth 
+    if (isBluetoothPresent == true) {
+
+        Bluetooth_Table.rows.item(1).cells.item(1).innerHTML = "Connecting";
+        const Device = navigator.bluetooth.requestDevice({
+            filters: [{
+                namePrefix: "DM"
+            }]
+        })
+            .then(async (device) => {
+                Bluetooth_Table.rows.item(0).cells.item(1).innerHTML = device.name
+                const connectedDevice = await device.gatt.connect();
+                bluetoothDevice = connectedDevice;
+                console.log(bluetoothDevice)
+                isConnected = true;
+                Bluetooth_Table.rows.item(1).cells.item(1).innerHTML = "CONNECTED";
+                button.innerHTML = "Disconnect"
+            })
+            .catch(error => { console.log(error); });
+    }
+}
+
+function Disconnect() {
+    // if (bluetoothDevice.device === undefined) {
+    console.log("Disconnecting Bluetooth from ", bluetoothDevice.name);
+    bluetoothDevice.disconnect();
+    console.log("Bluetooth Disconnected");
+    isConnected = false;
+    Bluetooth_Table.rows.item(0).cells.item(1).innerHTML = "Not Connected";
+    Bluetooth_Table.rows.item(1).cells.item(1).innerHTML = "Disconnected";
+    button.innerHTML = "Connect Bluetooth Devices";
+    console.log(bluetoothDevice);
+    // }
+
+
 }
